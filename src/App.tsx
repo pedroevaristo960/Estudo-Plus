02ffdb7ebf10594cc12ea160febfe1bb7bdc9f2d
Dejanir Martins <dejanir.diogo@gmail.com>
2025-07-14 10:18:50 +0100

    Primeiro estado do projeto Estuda+

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home.jsx';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Button from "../components/Button";
+export default function Home() {
+    return(
+        <>
+              <h1 className="bg-blue-500">Welcome to the Estuda+</h1>
+              <p>This is a our aplication</p>
+              <div><Button text="Let's Start?!" /></div>
+              <footer>
+                <p>&copy; 2025  Estuda++</p>
+              </footer>
+            </>
+    );
+  };
